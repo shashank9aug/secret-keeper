eaa85f5dab6d31f43e4b7af3d8ab64c02132295d
shashank <shekhar1245shashank@gmail.com>
2021-04-03 00:48:10 +0530

    applied OAuth2.0 authorisation cookies-session

--- README.md
+++ README.md
@@ -25,9 +25,18 @@ views             # all frontend  files.(hbs)
  - Authentication
  - cookies and sessions
 ## About:
------
-### Encryption
-  - Level-01 : <p>we simply store user credentials through signup form and store it in database. If user try to login we should validate their credientials </p> 
-  - Level-02 : 
-     
+### Authentication and Encryption
+  - Level-01 : We simply store user details in database and authenticate while user try to login.
+  problem arises if anybody have database access can get user details easily.
+
+  - Level-02 (encrypt) : we use encryption    technique as Caesar-Ciphar and an npm package mongoose-encryption.problem arises if anybody have db access and encryption key access can get user details easily as user details stored as binary.
+
+  - Level-03 (Hashing) : we use hashing to hash the password when user signup and hash the password when user try login. for this we use npm package 'md5' as it will generate hash.problem arises as hash value not change for fixed string.we may face dictionary attack by hacker's.
+
+  - Level-04 (Hashing and Salting) : when user signup user details will go through hash function with salting(by random set of string) to generate hash.for this we should use npm package becrypt.this method of encryption is far more secure than level 3.
+
+ - Level-05 (Cookies and Sessions)            
+  
+ - Level-06 (OAuth 2.0) :    
+
       
--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require("body-parser")
 const hbs = require('hbs')
 const mongoose = require("mongoose")
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -16,62 +25,166 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true})
+//express-session
+app.use(session({
+    secret: "This is our little secret.",
+    resave:false,
+    saveUninitialized: false
+}));
 
-const userSchema = {
-    name:String,
+//passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true})
+mongoose.set("useCreateIndex",true)
+
+const userSchema = new mongoose.Schema({
     email:String,
-    password:String
-}
+    password:String,
+    googleId:String
+})
+
+//plugin will hash and salt the password then store it in db
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const User = new mongoose.model("User",userSchema)
 
+passport.use(User.createStrategy())
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
+/*
+ there is so much difference between https and http
+   make sure in mind
+
+ */  
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/secrets',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile)
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/',(req,res)=>{
     res.render("home")
 })
+
+
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] })
+)
+
+app.get("/auth/google/secrets",
+ passport.authenticate('google', { failureRedirect: '/login'}),
+ function(req,res) {
+     //successfull authentication
+     res.redirect("/secrets");
+});
+
+
 app.get('/login',(req,res)=>{
     res.render("login")
 })
+
 app.get('/register',(req,res)=>{
     res.render("register")
 })
 
-app.post('/register',(req,res)=>{
-    const newUser = new User({
-        name:req.body.username,
-        email:req.body.email,
-        password:req.body.password
-    })
+app.get("/secrets",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    } else {
+        res.redirect("/login");
+    }
+});
 
-    newUser.save((err)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("secrets")
-        }   
+app.get("/logout",(req,res)=>{
+    req.logout()
+    res.redirect("/")
+})
+
+app.post('/register',(req,res)=>{
+//     const newUser = new User({
+//         name:req.body.username,
+//         email:req.body.email,
+//         password:req.body.password
+//     })
+
+//     newUser.save((err)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         else{
+//             res.render("secrets")
+//         }   
+//     })
+     
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+       if(err) {
+           console.log(err);
+           res.redirect("/register")
+       }    
+       else{
+           passport.authenticate("local")(req, res, function(){
+               res.redirect("/secrets");
+           })
+       }    
     })
 })
 
 app.post("/login",(req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
+//     const username = req.body.username;
+//     const password = req.body.password;
 
-    User.findOne({email: username}, (err, foundUser)=>{
-        if(err){
+//     User.findOne({email: username}, (err, foundUser)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             if(foundUser){
+//                 if(foundUser.password === password){
+//                     res.render("secrets");
+//                 }
+//             }
+//         }
+//     })
+    
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
+    });
+    
+    req.login(user,(err)=>{
+        if(err) {
             console.log(err);
         }
         else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
-            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
         }
     })
 })
 
 app.listen(3000,()=>{
-    console.log("Server started on https://localhost:3000")
+    console.log("Server started on http://localhost:3000")
 })
